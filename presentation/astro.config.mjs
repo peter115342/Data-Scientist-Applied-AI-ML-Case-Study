3fc29735ede7d72e8b4b5aa8718b9ea5a141ceb9
peter115342 <pmuzslay8@gmail.com>
2026-07-23 18:32:25 +0200

    feat: astro deployment pipeline, presentation color rework

--- presentation/astro.config.mjs
+++ presentation/astro.config.mjs
@@ -2,4 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
+  site: "https://peter115342.github.io",
+  base: "/Data-Scientist-Applied-AI-ML-Case-Study",
 });
